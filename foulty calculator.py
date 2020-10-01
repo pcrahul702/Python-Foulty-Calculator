2865d72d57f3b8493c641371eb815f14d43bc73b
Rahul kumar <pcrahul702@gmail.com>
2020-10-01 16:46:27 +0530

    version1

---
+++ foulty calculator.py
@@ -0,0 +1,25 @@
+var1=input("enter the operent")
+var2=int(input("Enter first number"))
+var3=int(input("Enter second number"))
+if var1=="+" :
+    if var2 ==56 and var3 ==9:
+        print(77)
+    else:
+        print(var2+var3)
+elif var1 =="*":
+    if var2 ==45 and var3 ==3:
+        print(555)
+    else:
+        print(var2*var3)
+elif  var1 == "/" :
+    if var2==56 and var3==6:
+        print(4)
+    else :
+
+        print(var2/var3)
+elif var1=="_":
+
+    print(var2-var3)
+else:
+    print("enter valid opretor")
+
